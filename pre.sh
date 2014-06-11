a57c5a911e81e1d80c4f17d71ee9adb1f0ef93fb
Ravil Mustafin <ravil@mustaf.in>
2014-06-11 12:38:54 +0400

    Added all needed packages

--- pre.sh
+++ pre.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#sudo apt-get update
+sudo apt-get update
 echo "Installing git-core..."
 sudo apt-get install -y git-core >/dev/null 2>&1
 
@@ -23,7 +23,13 @@ sudo apt-get install -y texlive\.\*cyr\.\* >/dev/null 2>&1
 echo "Installing texlive\.\*extra ..."
 sudo apt-get install -y texlive\.\*extra  >/dev/null 2>&1
 
-
 echo "Installing scalable-cyrfonts-tex..."
 sudo apt-get install -y scalable-cyrfonts-tex >/dev/null 2>&1
 #ln -fs /vagrant /var/www
+echo "Installing mathtex..."
+
+sudo apt-get install  -y mathtex >/dev/null 2>&1
+echo "Installing icomma..."
+sudo apt-get install -y  icomma >/dev/null 2>&1
+echo "Installing texlive-lang-latin..."
+sudo apt-get install -y  texlive-lang-latin >/dev/null 2>&1
